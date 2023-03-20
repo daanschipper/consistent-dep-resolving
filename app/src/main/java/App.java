e9897cc526539bf6ac034968e596e814ec65ef21
Daan Schipper <2778477+daanschipper@users.noreply.github.com>
2023-03-20 13:19:59 +0100

    Set up multi-project build

---
+++ app/src/main/java/App.java
@@ -0,0 +1,9 @@
+import io.vertx.core.Launcher;
+
+public class App {
+    public static void main(String[] args) {
+        // Mock usage of vertx-core
+        //noinspection unused
+        var launcherClass = Launcher.class;
+    }
+}
---
+++ lib/src/main/java/Lib.java
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Lib {
+    public static void main(String[] args) {
+        // Mock usage of jackson-databind
+        //noinspection unused
+        var objectMapperClass = ObjectMapper.class;
+    }
+}
